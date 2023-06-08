1bee98f4382395570e5b5bd2224e3052a63ecc5f
CarlosMdza <135670813+CarlosMdza@users.noreply.github.com>
2023-06-07 22:56:51 -0700

    Ventana tratamiento

--- src/Tratamiento.java
+++ src/Tratamiento.java
@@ -4,15 +4,17 @@ import java.util.Date;
 
 public class Tratamiento extends Mascota{
     protected String Medicinas;
-    protected Date Fecha,Duracion;
+    protected Date Fecha, Duracion;
+    protected int id;
 
     public Tratamiento() {
     }
     
-    public Tratamiento(String Medicinas, Date Fecha, Date Duracion) {
+    public Tratamiento(String Medicinas, Date Fecha, Date Duracion, int id) {
         this.Medicinas = Medicinas;
         this.Fecha = Fecha;
         this.Duracion = Duracion;
+        this.id = id;
     }
 
     public String getMedicinas() {
@@ -37,6 +39,14 @@ public class Tratamiento extends Mascota{
 
     public void setDuracion(Date Duracion) {
         this.Duracion = Duracion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
     
     
